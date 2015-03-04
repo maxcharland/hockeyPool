bd74038b8f7ffbd6774ab521d57037f96e880b87
Maxime Charland <maxime.charland@hybris.com>
2015-03-04 15:54:30 -0500

    add the menu

--- hockey/readsalary.go
+++ hockey/readsalary.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+func printMenu() {
+  fmt.Printf("1- Load Salary from nhlnumbers.\n")
+  fmt.Printf("2- Load stats from nhl.\n")
+  fmt.Printf("3- Get the top 10 best valuable players.\n")
+}
+
+
 func main() {
-	fmt.Printf("Read Salary from nhlnumbers.\n")
+	fmt.Printf("\n\n\nWelcome to hockey pool evaluator!\n")
+
+  printMenu()
+  var i int
+  fmt.Scanf("%d", &i)
+
+  fmt.Printf("Your choice is %d \n\n", i)
 }
