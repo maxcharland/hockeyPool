9235d68840b3cfaf2eda20244cf4b8e360858c93
Maxime Charland <maxime.charland@hybris.com>
2015-03-04 16:42:57 -0500

    Menu is ready

--- hockey/readsalary.go
+++ hockey/readsalary.go
@@ -2,19 +2,35 @@ package main
 
 import "fmt"
 
-func printMenu() {
-  fmt.Printf("1- Load Salary from nhlnumbers.\n")
-  fmt.Printf("2- Load stats from nhl.\n")
-  fmt.Printf("3- Get the top 10 best valuable players.\n")
-}
+func printMenu() int {
+  fmt.Println("1- Load Salary from nhlnumbers.")
+  fmt.Println("2- Load stats from nhl.")
+  fmt.Println("3- Get the top 10 best valuable players.")
+  fmt.Println("4- Quit.")
 
-
-func main() {
-	fmt.Printf("\n\n\nWelcome to hockey pool evaluator!\n")
-
-  printMenu()
   var i int
   fmt.Scanf("%d", &i)
-
   fmt.Printf("Your choice is %d \n\n", i)
+
+  return i
+}
+
+func main() {
+	fmt.Println("Welcome to hockey pool evaluator!")
+
+  var choice = printMenu()
+
+  for choice != 4 {
+    switch choice {
+    case 1:
+      fmt.Println("Loading salary")
+    case 2:
+      fmt.Println("Loading stats")
+    case 3:
+      fmt.Println("The top 10 is ...")
+    }
+
+    choice = printMenu()
+  }
+  fmt.Println("Bye!")
 }
